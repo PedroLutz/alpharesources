93e0d6b97868e6e7530da64d68e31a50ee6c12be
Pedro Lutz <pedrolutz@protonmail.com>
2023-09-24 13:51:43 -0300

    mudanca

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
 type ConnectionStatus = {
   isConnected: boolean
+}
+
+type Restaurant = {
+  _id: string;
+  name: string;
+  cuisine: string;
 }
 
 export const getServerSideProps: GetServerSideProps<
@@ -34,6 +41,22 @@ export const getServerSideProps: GetServerSideProps<
 export default function Home({
   isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [restaurants, SetRestaurants] = useState<Restaurant[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const results = await fetch("/api/list");
+        const resultsJson = await results.json();
+        SetRestaurants(resultsJson);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -46,50 +69,14 @@ export default function Home({
           Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
         </h1>
 
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
-        )}
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <div className = "grid">
+        {restaurants.map((restaurant) => (
+          <div className="card" key={restaurant._id}>
+            <h2>{restaurant.name}</h2>
+            <p>{restaurant.cuisine}</p>
+          </div>
+        ))}
+      </div>
       </main>
 
       <footer>
