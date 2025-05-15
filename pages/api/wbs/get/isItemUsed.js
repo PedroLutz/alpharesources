c1ba5f08a02c250f9cae7dbaebf16fc36166ee9d
Pedro Lutz <pedrolutz@protonmail.com>
2025-05-15 01:06:57 -0300

    adicionado tabelas a serem verificadas se tem items da wbs usados

--- pages/api/wbs/get/isItemUsed.js
+++ pages/api/wbs/get/isItemUsed.js
@@ -2,10 +2,16 @@ import connectToDatabase from '../../../../lib/db';
 import PlanoAquisicaoModel from '../../../../models/recursos/PlanoAquisicao';
 import RaciModel from '../../../../models/responsabilidade/Raci'
 import GanttModel from '../../../../models/Gantt';
+import RecursoModel from '../../../../models/recursos/Recurso';
+import RiscoModel from '../../../../models/riscos/Risco';
+import WbsDictionaryModel from '../../../../models/wbs/wbsDictionary';
 
 const { Raci } = RaciModel;
 const { PlanoAquisicao } = PlanoAquisicaoModel;
 const { Gantt } = GanttModel;
+const { Recurso } = RecursoModel;
+const { Risco } = RiscoModel;
+const { WbsDictionary } = WbsDictionaryModel;
 
 export default async (req, res) => {
     try {
@@ -14,10 +20,15 @@ export default async (req, res) => {
         if (req.method === 'POST') {
             const {area, item} = req.body;
             const plano = await PlanoAquisicao.findOne({ area: area, item: item });
-            const elementoWBS = await Raci.findOne({ area: area, item: item });
+            const raci = await Raci.findOne({ area: area, item: item });
             const gantt = await Gantt.findOne({ area: area, item: item });
+            const recurso = await Recurso.findOne({ area: area, item: item });
+            const risco = await Risco.findOne({ area: area, item: item });
+            const wbsDictionary = await WbsDictionary.findOne({ area: area, item: item });
 
-            if (elementoWBS !== null || plano !== null || gantt !== null) {
+            if (raci !== null || plano !== null || 
+                gantt !== null || recurso !== null ||
+                risco !== null || wbsDictionary !== null    ) {
                 res.json({ found: true });
             } else {
                 res.json({ found:false });
