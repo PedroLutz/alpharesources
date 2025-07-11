b93aa86dc583b4d4645d138fb144d65c713cc326
Pedro Lutz <pedrolutz@protonmail.com>
2025-07-11 12:04:55 -0300

    removido div aleatorio de carregando

--- components/ui/Loading.js
+++ components/ui/Loading.js
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
         <div className="overlay">
-            <div className="modal">
+            <div className="modal centered-container">
                 <img src='/images/alphie.png' style={{width: '300px', height: '200px', objectFit: 'cover'}}/>
                 <p>Wait a little while we set things up...</p>
             </div>
--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -5,7 +5,6 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [autenticado, setAutenticado] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [loading, setLoading] = useState(true); // pra evitar piscar
 
   useEffect(() => {
     const checarToken = async () => {
@@ -24,15 +23,11 @@ export const AuthProvider = ({ children }) => {
         console.error('erro ao verificar token', err);
         setAutenticado(false);
         setIsAdmin(false);
-      } finally {
-        setLoading(false); // quando terminar a verificação
-      }
+      } 
     };
 
     checarToken();
   }, []);
-
-  if (loading) return <div>carregando...</div>; // evita piscar conteúdo
 
   return (
     <AuthContext.Provider value={{ autenticado, setAutenticado, isAdmin, setIsAdmin }}>
