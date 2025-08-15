d4bf247a43e1efa74a4fc7b7e83c7b5a80f1c892
Pedro Lutz <pedrolutz@protonmail.com>
2025-08-15 12:10:39 -0300

    corrigida importacao do Router no LogoutButton

--- components/ui/LogoutButton.js
+++ components/ui/LogoutButton.js
@@ -1,6 +1,9 @@
 import client from "../../lib/supabaseClient";
+import { useRouter } from 'next/router';
 
 const Logout = () => {
+    const router = useRouter();
+
     return (
         <button className="botao_logout" onClick={async () => {
             await client.auth.signOut();
