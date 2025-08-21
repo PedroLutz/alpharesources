0da6f76bee0ca5b9bac65401d07609205e053405
Pedro Lutz <pedrolutz@protonmail.com>
2025-08-21 20:03:23 -0300

    iniciado conversao para supabase em cronograma

---
+++ components/features/n_cronograma/CadastroInputs.js
@@ -0,0 +1,275 @@
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { fetchData } from "../../../functions/crud";
+import styles from '../../../styles/modules/cronograma.module.css';
+
+const CadastroInputs = ({ tipo, obj, objSetter, funcoes, setExibirModal, gantt }) => {
+    const [elementosWBS, setElementosWBS] = useState([]);
+    const [itensPorArea, setItensPorArea] = useState({ notDp: [], dp: [] });
+    const camposRef = useRef({
+        item: null,
+        area: null,
+        inicio: null,
+        termino: null,
+        dp_item: null,
+        dp_area: null,
+        situacao: null,
+    });
+    const { isAdmin } = useContext(AuthContext);
+
+    //funcao para inserir os dados dos inputs para o objeto
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        objSetter({
+            ...obj,
+            [name]: value,
+        });
+        e.target.classList.remove('campo-vazio');
+    };
+
+    const checarValidezDosDados = async (item) => {
+        const response = await fetch('/api/cronograma/checks/checkData', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(item)
+        });
+        const data = await response.json();
+        return [data.invalido, data.problema];
+    };
+
+    //funcao para verificar entre diversos casos para validar os dados
+    const validaDados = async () => {
+        let camposConsiderados;
+        if (tipo === 'update' || tipo === 'updatemonitoring') {
+            camposConsiderados = {
+                inicio: obj.inicio,
+                termino: obj.termino,
+                situacao: obj.situacao
+            }
+        } else if (tipo === 'cadastro') {
+            camposConsiderados = {
+                area: obj.area,
+                item: obj.item,
+                inicio: obj.inicio,
+                termino: obj.termino,
+            }
+        }
+
+        const camposVazios = Object.entries(camposConsiderados)
+            .filter(([key, value]) => value === null || value === "")
+            .map(([key]) => key);
+
+        if (camposVazios.length > 0) {
+            camposVazios.forEach(campo => {
+                if (camposRef.current[campo]) {
+                    camposRef.current[campo].classList.add('campo-vazio');
+                }
+            });
+            setExibirModal('inputsVazios');
+            return true;
+        }
+        var objEnviado = obj;
+        if (obj.dp_area == undefined && obj.dp_item == undefined) {
+            objEnviado = {
+                ...obj,
+                dp_area: '',
+                dp_item: ''
+            }
+        }
+        if (obj.inicio === "1970-01-01" && obj.termino === "1970-01-01") {
+            objEnviado = {
+                ...obj,
+                inicio: '',
+                termino: ''
+            }
+        }
+        if (obj.inicio > obj.termino) {
+            setExibirModal('datasErradas');
+            return true;
+        }
+
+        const [isInvalido, problema] = await checarValidezDosDados(objEnviado);
+        if (isInvalido) {
+            setExibirModal(problema);
+            return true;
+        }
+        if (obj.dp_area && !obj.dp_item) {
+            setExibirModal('depFaltando');
+            return true;
+        }
+    }
+
+
+    //funcao para atualizar os itens no select apos a selecao de uma area (seja dp ou nao)
+    const handleAreaChange = (e, isDp) => {
+        const areaSelecionada = e.target.value;
+        const itensDaArea = elementosWBS
+            .filter(item => item.area === areaSelecionada).map(item => item.item);
+        if (!isDp) {
+            setItensPorArea({ ...itensPorArea, notDp: itensDaArea });
+        } else {
+            setItensPorArea({ ...itensPorArea, dp: itensDaArea });
+        }
+        handleChange(e);
+    };
+
+
+    //funcao para buscar os elementos da WBS para inserção nos selects
+    const fetchElementos = async () => {
+        const data = await fetchData('wbs/get/all');
+        setElementosWBS(data.elementos);
+    };
+
+
+    //useEffect que roda apenas na primeira execucao
+    useEffect(() => {
+        fetchElementos();
+    }, []);
+
+    //useEffect que so executa quando obj.dp_area atualiza que apaga
+    //o valor de dp_item caso dp_area for vazio
+    useEffect(() => {
+        if (obj.dp_area == '') {
+            objSetter({
+                ...obj,
+                dp_item: ''
+            })
+        }
+    }, [obj.dp_area]);
+
+
+    //so executa quando o tipo for cadastro pq a atualizacao n altera nem a area nem o item
+    //ent n pode mexer no obj
+    useEffect(() => {
+        if (tipo == 'cadastro') {
+            objSetter({
+                ...obj,
+                item: ''
+            })
+        }
+    }, [obj.area]);
+
+
+    //funcao que valida os dados e executa ou nao a funcao de submit
+    const handleSubmit = async () => {
+        funcoes?.setLoading(true);
+        const isInvalido = await validaDados();
+        funcoes?.setLoading(false);
+        if (isInvalido) return;
+        funcoes?.enviar();
+    }
+
+    return (
+        <React.Fragment>
+            {tipo === 'cadastro' && (
+                <React.Fragment>
+                    <td>
+                        <select
+                            name="area"
+                            onChange={(e) => handleAreaChange(e, false)}
+                            value={obj.area}
+                            ref={el => (camposRef.current.area = el)}
+                        >
+                            <option value="" defaultValue>Area</option>
+                            {[...new Set(elementosWBS.map(item => item.area))].map((area, index) => (
+                                <option key={index} value={area}>{area}</option>
+                            ))};
+                        </select>
+                    </td>
+                    <td>
+                        <select
+                            name="item"
+                            onChange={handleChange}
+                            value={obj.item}
+                            ref={el => (camposRef.current.item = el)}
+                        >
+                            <option value="" defaultValue>Item</option>
+                            {itensPorArea.notDp.map((item, index) => (
+                                <option key={index} value={item}>{item}</option>
+                            ))}
+                        </select>
+                    </td>
+                </React.Fragment>
+            )}
+            <td>
+                <input
+                    type="date"
+                    name="inicio"
+                    onChange={handleChange}
+                    value={obj.inicio}
+                    ref={el => (camposRef.current.inicio = el)}
+                />
+            </td>
+            <td>
+                <input
+                    type="date"
+                    name="termino"
+                    onChange={handleChange}
+                    value={obj.termino}
+                    ref={el => (camposRef.current.termino = el)}
+                />
+            </td>
+            {tipo !== 'updatemonitoring' ? (
+                <React.Fragment>
+                    <td>
+                        <select
+                            name="dp_area"
+                            onChange={(e) => handleAreaChange(e, true)}
+                            value={obj.dp_area}
+                            ref={el => (camposRef.current.dp_area = el)}
+                        >
+                            <option value="" defaultValue>None</option>
+                            {[...new Set(elementosWBS.map(item => item.area))].map((area, index) => (
+                                <option key={index} value={area}>{area}</option>
+                            ))};
+                        </select>
+                    </td>
+                    <td>
+                        <select
+                            name="dp_item"
+                            onChange={handleChange}
+                            value={obj.dp_item}
+                            ref={el => (camposRef.current.dp_item = el)}
+                        >
+                            <option value="" defaultValue>None</option>
+                            {itensPorArea.dp.map((item, index) => (
+                                <option key={index} value={item}>{item}</option>
+                            ))}
+                        </select>
+                    </td>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <td>{obj.dp_area || '-'}</td>
+                    <td>{obj.dp_item || '-'}</td>
+                </React.Fragment>
+            )}
+
+            {gantt && (
+                <td>
+                    <select
+                        name="situacao"
+                        onChange={handleChange}
+                        value={obj.situacao}
+                        ref={el => (camposRef.current.situacao = el)}
+                    >
+                        <option value="iniciar">Starting</option>
+                        <option value="em andamento">Executing</option>
+                        <option value="concluida">Completed</option>
+                    </select>
+                </td>
+            )}
+            <td className={tipo !== 'cadastro' ? 'botoes_acoes' : undefined}>
+                {tipo === 'cadastro' ? (
+                    <button className={styles.botaoCadastro} disabled={!isAdmin} onClick={(e) => handleSubmit(e)}>Add new</button>
+                ) : (
+                    <React.Fragment>
+                        <button onClick={handleSubmit}>✔️</button>
+                        <button onClick={funcoes?.cancelar}>✖️</button>
+                    </React.Fragment>
+                )}
+            </td>
+        </React.Fragment>
+    )
+}
+
+export default CadastroInputs;
---
+++ components/features/n_cronograma/Tabela.js
@@ -0,0 +1,443 @@
+import React, { useEffect, useState, useContext, useMemo } from 'react';
+import Loading from '../../ui/Loading';
+import Modal from '../../ui/Modal';
+import { Chart } from 'react-google-charts';
+import { fetchData, handleDelete, handleUpdate, handleSubmit } from '../../../functions/crud';
+import { cleanForm, jsDateToEuDate, euDateToIsoDate, euDateToJsDate } from '../../../functions/general';
+import styles from '../../../styles/modules/cronograma.module.css';
+import CadastroInputs from './CadastroInputs';
+import chroma from 'chroma-js';
+import useAuth from '../../../hooks/useAuth';
+
+const Tabela = () => {
+  const { user, token } = useAuth();
+  const [cronogramas, setCronogramas] = useState([]);
+  const [cronogramasCont, setCronogramasCont] = useState([]);
+  const [deleteSuccess, setDeleteSuccess] = useState(false);
+  const [confirmDeleteItem, setConfirmDeleteItem] = useState(null);
+  const [exibirModal, setExibirModal] = useState(null);
+  const [chartHeight, setChartHeight] = useState('100px');
+  const [chartDataLoaded, setChartDataLoaded] = useState(false);
+  const [linhaVisivel, setLinhaVisivel] = useState({});
+  const [reload, setReload] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [cores, setCores] = useState({});
+  const camposVazios = {
+    item: '',
+    area: '',
+    inicio: '',
+    termino: '',
+    dp_item: '',
+    dp_area: '',
+  }
+  const camposSubmit = {
+    id: '',
+    item_id: '',
+    is_plan: '',
+    start: '',
+    end: '',
+    status: '',
+    user_id: ''
+  }
+  const [novosDados, setNovosDados] = useState(camposVazios);
+  const [novoSubmit, setNovoSubmit] = useState(camposVazios);
+  const [paleta, setPaleta] = useState([]);
+  const [etis, setEtis] = useState([]);
+  const [showContingencies, setShowContingencies] = useState(false);
+  const [tabela, setTabela] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
+
+
+  //funcao que recebe o item a ser atualizado e insere os campos relevantes em novosDados
+  const handleUpdateClick = (item) => {
+    setNovosDados({
+      _id: item._id,
+      plano: true,
+      inicio: euDateToIsoDate(item.inicio),
+      termino: euDateToIsoDate(item.termino),
+      dp_item: item.dp_item || undefined,
+      dp_area: item.dp_area || undefined,
+      situacao: item.situacao,
+    });
+  };
+
+
+  //funcao que deleta o item tanto no plano quanto no monitoramento
+  const handleConfirmDelete = async () => {
+    setConfirmDeleteItem(null);
+    if (confirmDeleteItem) {
+      var getDeleteSuccess = false;
+      try {
+        getDeleteSuccess = await handleDelete({
+          route: 'cronograma',
+          item: confirmDeleteItem,
+          fetchDados: fetchCronogramas
+        });
+      } finally {
+        setDeleteSuccess(getDeleteSuccess);
+      }
+    }
+  };
+
+
+  //funcao para somar dias a uma data
+  function adicionarDias(data, dias) {
+    if (dias) {
+      const novaData = new Date(data);
+      novaData.setDate(novaData.getDate() + dias);
+      return novaData;
+    }
+    return data;
+  }
+
+
+  //funcao para criar os dados que entram no grafico gantt, tanto o normal quanto o de contingencia
+  const createGanttData = () => {
+    const ganttData = [['Task ID', 'Task Name', 'Resource', 'Start Date', 'End Date', 'Duration', 'Percent Complete', 'Dependencies']];
+    const ganttDataContingency = [['Task ID', 'Task Name', 'Resource', 'Start Date', 'End Date', 'Duration', 'Percent Complete', 'Dependencies']];
+
+    cronogramas.forEach((item) => {
+      if (item.plano) {
+        var dependencies = ''
+        const taskID = `${item.area}_${item.item}`;
+        const taskName = item.item;
+        const resource = item.area;
+        const startDate = euDateToJsDate(item.inicio);
+        const endDate = euDateToJsDate(item.termino);
+        if (!item.dp_area && !item.dp_item) {
+          dependencies = null;
+        } else {
+          dependencies = `${item.dp_area}_${item.dp_item}`;
+        }
+        ganttData.push([taskID, taskName, resource, startDate, endDate, 10, 100, dependencies]);
+        ganttDataContingency.push([taskID, taskName, resource, startDate, adicionarDias(endDate, Math.floor(etis[taskName])), 10, 100, dependencies]);
+      }
+    });
+
+    return [ganttData, ganttDataContingency];
+  };
+
+  //funcao que executa na primeira render e depois so quando cronogramas ou etis atualiza
+  //armazenando os dados diretamente nas constantes
+
+  //useMemo ARMAZENA VALOR
+  //useEffect REALIZA FUNCOES
+  const [chartData, chartDataContingencies] = useMemo(() => {
+    if (cronogramas.length === 0) return [[], []];
+    return createGanttData();
+  }, [cronogramas, etis]);
+
+
+  //funcao para puxar os dados de cronograma, ETIs e cores, tratando-os e armazenando-os em estados
+  const fetchCronogramas = async () => {
+    try {
+      const data = await fetchData('cronograma/get/planos');
+      const dataETIs = await fetchData('riscos/analise/get/etis_per_item');
+      const dataCores = await fetchData('wbs/get/cores');
+
+      data.cronogramaPlanos.forEach((item) => {
+        item.inicio = jsDateToEuDate(item.inicio);
+        item.termino = jsDateToEuDate(item.termino);
+      });
+      data.cronogramaPlanos.sort((a, b) => {
+        if (a.area < b.area) return -1;
+        if (a.area > b.area) return 1;
+        return 0;
+      });
+
+      const cronogramaComContingencias = data.cronogramaPlanos.map(item => ({ ...item }));;
+      cronogramaComContingencias.forEach((item) => {
+        if (dataETIs.resultadosAgrupados[item.item]) {
+          const termino = euDateToJsDate(item.termino)
+          const terminoConvertido = adicionarDias(termino, Math.floor(dataETIs.resultadosAgrupados[item.item]));
+          item.termino = jsDateToEuDate(terminoConvertido);
+        }
+      })
+
+      //adicionar cores na tabela
+      var cores = {};
+      dataCores.areasECores.forEach((area) => {
+        cores = { ...cores, [area._id]: area.cor[0] ? area.cor[0] : '' }
+      })
+
+      //adicionar cores no grafico (apenas as cores de areas que tem alguma coisa sendo executada)
+      var paleta = [];
+      for (const [key, value] of Object.entries(cores)) {
+        if (data.cronogramaPlanos.some((item) => item.area === key && item.termino !== null)) {
+          paleta.push({
+            "color": value ? chroma(value).darken().saturate(3).hex() : '#000000',
+            "dark": value ? chroma(value).hex() : '#000000',
+            "light": value ? chroma(value).darken().hex() : '#000000'
+          })
+        }
+      }
+
+      setCores(cores);
+      setPaleta(paleta);
+      setEtis(dataETIs.resultadosAgrupados);
+      setCronogramas(data.cronogramaPlanos);
+      setCronogramasCont(cronogramaComContingencias);
+      setTabela(data.cronogramaPlanos);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const modalLabels = {
+    'inputsVazios': 'Fill out all fields before adding new data!',
+    'dadosUsados': 'This item is already registered in the timelines!',
+    'depFaltando': 'Please select the dependencies correctly!',
+    'dpNotRegistered': "The item you've selected as predecessor is not registered!",
+    'dpNotOkay': "The predecessor must finish before the successor starts!",
+    'datasErradas': 'The finishing date must be after the starting date!'
+  };
+
+
+  //useEffect que só executa as funcoes quando reload atualiza
+  useEffect(() => {
+    if(reload == true){
+      setReload(false);
+      fetchCronogramas();
+    }
+  }, [reload]);
+
+
+  //useEffect que so roda na primeira execucao
+  useEffect(() => {
+    fetchCronogramas();
+  }, []);
+
+
+  //useEffect que só executa quando chartData recebe um valor para calcular o tamanho do grafico
+  useEffect(() => {
+    if (chartData.length > 1) {
+      const linhaHeight = isMobile ? 20 : 30;
+      const novaAltura = ((chartData.length * linhaHeight) + 50) + 'px';
+      setChartHeight(novaAltura);
+      setChartDataLoaded(true);
+    }
+  }, [chartData]);
+
+
+  //funcao que cadastra o plano e o monitoramento, com os dados vazios
+  const enviar = async () => {
+    const formDataPlano = {
+      ...novoSubmit,
+      plano: true,
+      situacao: 'concluida'
+    };
+
+    const formDataGantt = {
+      ...novoSubmit,
+      plano: false,
+      inicio: null,
+      termino: null,
+      situacao: 'iniciar'
+    };
+    await handleSubmit({
+      route: 'cronograma',
+      dados: formDataPlano,
+      fetchDados: fetchCronogramas
+    });
+    await handleSubmit({
+      route: 'cronograma',
+      dados: formDataGantt,
+      
+    });
+    cleanForm(novoSubmit, setNovoSubmit, camposVazios);
+  };
+
+  //funcao que trata os dados e atualiza o plano
+  const handleUpdateItem = async () => {
+    if (novosDados) {
+      try {
+        await handleUpdate({
+          route: 'cronograma',
+          dados: novosDados,
+          fetchDados: fetchCronogramas
+        });
+      } catch (error) {
+        console.error("Update failed:", error);
+      }
+    }
+    cleanForm(novosDados, setNovosDados, camposVazios);
+    setLinhaVisivel();
+  };
+
+  
+  //funcao que calcula o rowSpan do td da area de acordo com os itens 
+  const calculateRowSpan = (itens, currentArea, currentIndex) => {
+    let rowSpan = 1;
+    for (let i = currentIndex + 1; i < itens.length; i++) {
+      if (itens[i].area === currentArea) {
+        rowSpan++;
+      } else {
+        break;
+      }
+    }
+    return rowSpan;
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth < 1024) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="centered-container">
+      {loading && <Loading />}
+      <h2 className='smallTitle'>Estimated timeline</h2>
+      {confirmDeleteItem && (
+        <div className="overlay">
+          <div className="modal">
+            <p>Are you sure you want to delete "{confirmDeleteItem.area} - {confirmDeleteItem.item}"?</p>
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <button className="botao-padrao" onClick={handleConfirmDelete}>Confirm</button>
+              <button className="botao-padrao" onClick={() => setConfirmDeleteItem(null)}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {deleteSuccess && (
+        <div className="overlay">
+          <div className="modal">
+            <p>{deleteSuccess ? 'Deletion successful!' : 'Deletion failed.'}</p>
+            <button className="botao-padrao" onClick={() => setDeleteSuccess(false)}>Close</button>
+          </div>
+        </div>
+      )}
+
+      {exibirModal != null && (
+        <Modal objeto={{
+          titulo: modalLabels[exibirModal],
+          botao1: {
+            funcao: () => setExibirModal(null), texto: 'Okay'
+          },
+        }} />
+      )}
+
+      <button className="botao-bonito" style={{ width: '11rem' }} onClick={() => {
+        setShowContingencies(!showContingencies);
+        showContingencies ? setTabela(cronogramas) : setTabela(cronogramasCont);
+      }}
+      >{!showContingencies ? `Show contingencies` : `Hide contingencies`}</button>
+
+      {chartDataLoaded && (
+        <div style={{width: '90%', height: chartHeight}}>
+          <Chart
+          key={isMobile ? "mobile" : "desktop"}
+          height="100%"
+          width="100%"
+          chartType="Gantt"
+          loader={<div>Loading Chart</div>}
+          data={!showContingencies ? chartData : chartDataContingencies}
+          options={{
+            gantt: {
+              trackHeight: isMobile ? 20 : 30,
+              barHeight: isMobile ? 10 : null,
+              arrow: {
+                length: isMobile ? 0 : 8
+              },
+              sortTasks: false,
+              palette: paleta,
+              shadowEnabled: false,
+              criticalPathEnabled: false,
+              labelMaxWidth: isMobile ? 0 : 300
+            },
+          }}
+        />
+        </div>
+        
+      )}
+
+      <div className="centered-container">
+        <div className={styles.tabelaCronograma_container}>
+          <div className={styles.tabelaCronograma_wrapper}>
+            <table style={{ marginBottom: '10px' }} className={`tabela ${styles.tabelaCronograma}`}>
+              <thead>
+                <tr>
+                  <th>Area</th>
+                  <th>Task</th>
+                  <th>Start</th>
+                  <th>End</th>
+                  <th>Dependency: Area</th>
+                  <th>Dependency: Item</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <CadastroInputs
+                    obj={{ ...novoSubmit, plano: true }}
+                    objSetter={setNovoSubmit}
+                    tipo='cadastro'
+                    funcoes={{
+                      enviar,
+                      setLoading
+                    }}
+                    setExibirModal={setExibirModal}
+                  />
+                </tr>
+                {tabela.filter((item) => item.plano).map((item, index) => (
+                  <tr key={index} style={{ backgroundColor: cores[item.area] }}>
+                    <React.Fragment>
+                      {index === 0 || cronogramas[index - 1].area !== item.area ? (
+                        <td rowSpan={calculateRowSpan(cronogramas, item.area, index)}
+                        >{item.area}</td>
+                      ) : null}
+                      <td>
+                        {item.item}
+                      </td>
+                      {linhaVisivel === item._id ? (
+                        <CadastroInputs
+                          tipo="update"
+                          obj={novosDados}
+                          objSetter={setNovosDados}
+                          setExibirModal={setExibirModal}
+                          funcoes={{
+                            enviar: handleUpdateItem,
+                            setLoading,
+                            cancelar: () => linhaVisivel === item._id ? setLinhaVisivel() : setLinhaVisivel(item._id)
+                          }}
+                        />
+                      ) : (
+                        <React.Fragment>
+                          <td>{item.inicio}</td>
+                          <td>{item.termino}</td>
+                          <td>{item.dp_area || '-'}</td>
+                          <td>{item.dp_item || '-'}</td>
+                          <td className="botoes_acoes">
+                            
+                              <button onClick={() => setConfirmDeleteItem(item)}
+                                disabled={!isAdmin}>❌</button>
+                              <button onClick={() => {
+                                linhaVisivel === item._id ? setLinhaVisivel() : setLinhaVisivel(item._id); handleUpdateClick(item)
+                              }}
+                                disabled={!isAdmin}>⚙️</button>
+                            
+                          </td>
+                        </React.Fragment>
+                      )}
+                    </React.Fragment>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Tabela;
