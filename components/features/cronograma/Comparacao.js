732797b99375adccbed0a4bf5a7b343c24254d75
Pedro Lutz <pedrolutz@protonmail.com>
2026-03-18 13:02:36 -0300

    melhorado grafico comparativo de gantt

--- components/features/cronograma/Comparacao.js
+++ components/features/cronograma/Comparacao.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Chart } from 'react-google-charts';
 import Loading from '../../ui/Loading';
 import { handleFetch } from '../../../functions/crud_s';
@@ -21,6 +21,7 @@ const Tabela = () => {
     const [paleta, setPaleta] = useState([]);
     const [isMobile, setIsMobile] = useState(false);
     const [showHelp, setShowHelp] = useState(false);
+    const [showDates, setShowDates] = useState(false);
 
     //useEffect que so executa quando acontece reload é atualizado
     useEffect(() => {
@@ -40,7 +41,8 @@ const Tabela = () => {
                 query: "monitors",
                 token
             });
-            data.data.sort((a, b) => {
+            const ganttMonitors = data.data;
+            ganttMonitors.sort((a, b) => {
                 if (a.wbs_item.wbs_area.name != b.wbs_item.wbs_area.name) {
                     return a.wbs_item.wbs_area.name > b.wbs_item.wbs_area.name
                 }
@@ -48,7 +50,7 @@ const Tabela = () => {
                 return isoDateToJsDate(a.gantt_data[0].start) > isoDateToJsDate(b.gantt_data[0].start)
             })
             const cronogramasShouldBeGraphed = [];
-            data.data.forEach((item) => {
+            ganttMonitors.forEach((item) => {
                 item.gantt_data[0].start = jsDateToEuDate(item?.gantt_data[0].start);
                 item.gantt_data[0].end = jsDateToEuDate(item?.gantt_data[0].end);
                 if (euDateToJsDate(item.gantt_data[0].start) < euDateToJsDate(item.gantt_data[0].end) &&
@@ -75,21 +77,25 @@ const Tabela = () => {
 
             //adicionar cores na tabela
             var cores = {};
-            data.data.forEach((c) => {
+            ganttMonitors.forEach((c) => {
                 cores = { ...cores, [c.wbs_item.wbs_area.name]: c.wbs_item.wbs_area.color ? c.wbs_item.wbs_area.color : '' }
             })
 
             var paleta = [];
             for (const [key, value] of Object.entries(cores)) {
-                if (data.data.some((item) => item.wbs_item.wbs_area.name === key && item.gantt_data[0].end !== null)) {
+                if (ganttMonitors.some((item) => item.wbs_item.wbs_area.name === key && item.gantt_data[0].end !== null)) {
                     paleta.push({
-                        "color": value ? chroma(value).darken().saturate(3).hex() : '#000000',
+                        "color": '#000000',
                         "dark": value ? chroma(value).hex() : '#000000',
-                        "light": value ? chroma(value).darken().hex() : '#000000'
+                        "light": value ? chroma(value).hex() : '#000000',
+                    })
+                    paleta.push({
+                        "color": '#000000',
+                        "dark": value ? chroma(value).darken(1).hex() : '#000000',
+                        "light": value ? chroma(value).darken(1).hex() : '#000000',
                     })
                 }
             }
-
             setPaleta(paleta);
         } finally {
             setLoading(false);
@@ -113,7 +119,7 @@ const Tabela = () => {
                     var dependencies2 = '';
                     const taskID2 = `${planoDoGantt.gantt_data[0].id}`;
                     const taskName2 = planoDoGantt.wbs_item.name;
-                    const resource2 = planoDoGantt.wbs_item.wbs_area.name;
+                    const resource2 = planoDoGantt.wbs_item.wbs_area.name + "p"; //p for planned
                     const startDate2 = euDateToJsDate(planoDoGantt.gantt_data[0].start);
                     const endDate2 = euDateToJsDate(planoDoGantt.gantt_data[0].end);
                     if (!planoDoGantt.gantt_dependency[0]) {
@@ -127,7 +133,7 @@ const Tabela = () => {
                 var dependencies = '';
                 const taskID = `${item.gantt_data[0].id}`;
                 const taskName = item.wbs_item.name;
-                const resource = item.wbs_item.wbs_area.name;
+                const resource = item.wbs_item.wbs_area.name + "r"; //r for real
                 const startDate = euDateToJsDate(item.gantt_data[0].start);
                 const endDate = euDateToJsDate(item.gantt_data[0].end);
                 if (!item.gantt_dependency[0]) {
@@ -205,118 +211,174 @@ const Tabela = () => {
             {loading && <Loading />}
             {showHelp && <HelpBubble setShowHelp={setShowHelp} />}
             <h2 className='smallTitle'>Planned Schedule vs Reality <button onClick={() => setShowHelp(true)}>❔</button></h2>
+            <button className='botao-bonito' onClick={() => setShowDates(!showDates)}>{!showDates ? 'View dates' : 'Hide dates'}</button>
 
             {cronogramas.length > 0 ? (
                 <div className='centered-container' style={{
-                width: "50vw",
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                position: 'relative',
-            }}>
-                <table style={{
-                    marginBottom: '80px',
+                    width: "50vw",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    position: 'relative',
                 }}>
-                    <tbody style={{ borderColor: 'black', borderStyle: 'solid', borderWidth: '0.01rem' }}>
-                        {cronogramas
-                            .map((item, index) => (
-                                <tr key={index}
-                                    style={{
-                                        height: '30px',
+                    <table style={{
+                        marginBottom: '78px',
+                        borderBottom: "0.15rem solid black"
+                    }}>
+                        <tbody style={{ borderColor: 'black', borderStyle: 'solid', borderWidth: '0.01rem' }}>
+                            {cronogramas
+                                .map((item, index) => {
+                                    const isFirstOfArea = index === 0 || cronogramas[index - 1].wbs_item.wbs_area.id !== item.wbs_item.wbs_area.id;
+                                    const plan = planosCronogramas.find((item_) => item_.wbs_item.id === item.wbs_item.id);
+
+                                    const trStyle = {
+                                        height: showDates ? '15px' : '30px',
                                         borderColor: 'black',
                                         borderStyle: 'solid',
                                         borderWidth: '0.01rem',
                                         borderRightWidth: '0rem',
                                         backgroundColor: item.wbs_item.wbs_area.color,
-                                        color: getTextColor(item.wbs_item.wbs_area.color)
-                                    }}>
-                                    {index === 0 || cronogramas[index - 1].wbs_item.wbs_area.id !== item.wbs_item.wbs_area.id ? (
-                                        <td style={{ fontSize: tamanhoDaFonte(item.wbs_item.wbs_area.name.length < 28 ? item.wbs_item.wbs_area.name.length : reduceLabel(item.wbs_item.wbs_area.name).length), minWidth: '6rem', maxWidth: '8rem', borderRightWidth: '0.1rem', borderRightStyle: 'solid' }}
-                                            rowSpan={calculateRowSpan(item.wbs_item.wbs_area.id, index)}
-                                        >{item.wbs_item.wbs_area.name.length < 28 ? item.wbs_item.wbs_area.name : reduceLabel(item.wbs_item.wbs_area.name)}</td>
-                                    ) : null}
-                                    <td style={{ fontSize: tamanhoDaFonte(item.wbs_item.name.length < 28 ? item.wbs_item.name.length : reduceLabel(item.wbs_item.name).length), minWidth: '6rem', maxWidth: '8rem' }}>{item.wbs_item.name.length < 28 ? item.wbs_item.name : reduceLabel(item.wbs_item.name)}</td>
-                                </tr>
-                            ))}
-                    </tbody>
-                </table>
-                <div className="centered-container" style={{
-                    width: isMobile ? "70vw" : "40vw",
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "flex-start",
-                    position: 'relative',
-                }}>
+                                        color: getTextColor(item.wbs_item.wbs_area.color),
+                                    }
 
-                    <table style={{
-                        position: 'absolute',
-                        zIndex: 2,
-                        width: '100%'
-                    }}>
-                        <tbody>
-                            {cronogramas.map((_, index) => (
-                                <tr key={index}
-                                    style={{ height: '30px', borderColor: 'black', borderStyle: 'solid', borderWidth: '0.1rem', borderLeftWidth: '0rem' }}>
-                                    <td></td>
-                                </tr>
-                            ))}
+                                    const firstOfAreaStyle = isFirstOfArea && {
+                                        borderTopStyle: "solid",
+                                        borderTopWidth: "0.15rem"
+                                    };
+
+                                    const areaName = item.wbs_item.wbs_area.name;
+                                    const areaFontSize = tamanhoDaFonte(areaName.length < 28 ? areaName.length : reduceLabel(areaName).length);
+                                    const areaLabel = areaName.length < 28 ? areaName : reduceLabel(areaName);
+
+                                    const itemName = item.wbs_item.name;
+                                    const itemFontSize = tamanhoDaFonte(itemName.length < 28 ? itemName.length : reduceLabel(itemName).length);
+                                    const itemLabel = itemName.length < 28 ? itemName : reduceLabel(itemName);
+                                    return (
+                                        <React.Fragment key={index}>
+                                            <tr
+                                                style={trStyle}>
+                                                {isFirstOfArea ? (
+                                                    <td style={{
+                                                        fontSize: areaFontSize, minWidth: '6rem', maxWidth: '8rem', borderRightWidth: '0.1rem', borderRightStyle: 'solid',
+                                                        ...firstOfAreaStyle
+                                                    }}
+                                                        rowSpan={calculateRowSpan(item.wbs_item.wbs_area.id, index) * (showDates ? 2 : 1)}
+                                                    >{areaLabel}</td>
+                                                ) : null}
+                                                <td rowSpan={showDates ? 2 : 1} style={{ fontSize: itemFontSize, minWidth: '6rem', maxWidth: '8rem', ...firstOfAreaStyle }}
+                                                >{itemLabel}</td>
+                                                {showDates && (
+                                                    <>
+                                                        <td style={{ fontSize: '0.6rem', ...firstOfAreaStyle }}>Planned</td>
+                                                        <td style={{ fontSize: '0.6rem', ...firstOfAreaStyle }}>{plan?.gantt_data[0].start}</td>
+                                                        <td style={{ fontSize: '0.6rem', ...firstOfAreaStyle }}>{plan?.gantt_data[0].end}</td>
+                                                    </>
+                                                )}
+
+                                            </tr>
+                                            {showDates && (
+                                                <tr style={trStyle}>
+                                                    <td style={{ fontSize: '0.6rem' }}>Actual</td>
+                                                    <td style={{ fontSize: '0.6rem' }}>{item.gantt_data[0].start}</td>
+                                                    <td style={{ fontSize: '0.6rem' }}>{item.gantt_data[0].end}</td>
+                                                </tr>
+                                            )}
+
+                                        </React.Fragment>
+                                    )
+                                })}
                         </tbody>
                     </table>
+                    <div className="centered-container" style={{
+                        width: isMobile ? "70vw" : "40vw",
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "flex-start",
+                        position: 'relative',
+                    }}>
 
-                    {chartDataLoaded && (
-                        <Chart
-                            width={isMobile ? "70vw" : "40vw"}
-                            height={chartHeight}
-                            chartType="Gantt"
-                            loader={<div>Loading Chart</div>}
-                            data={chartData}
-                            options={{
-                                gantt: {
-                                    trackHeight: 15,
-                                    sortTasks: false,
-                                    palette: paleta,
-                                    shadowEnabled: false,
-                                    criticalPathEnabled: false,
-                                    barHeight: 10,
-                                    arrow: {
-                                        angle: 100,
-                                        length: 4,
-                                        width: 1,
-                                        color: 'black',
-                                        radius: 0
+                        <table style={{
+                            position: 'absolute',
+                            zIndex: 2,
+                            width: '100%',
+                            borderBottom: "0.15rem solid black",
+                        }}>
+                            <tbody>
+                                {cronogramas.map((item, index) => {
+                                    const isFirstOfArea = index === 0 || cronogramas[index - 1].wbs_item.wbs_area.id !== item.wbs_item.wbs_area.id;
+                                    return (
+                                        <tr key={index}
+                                            style={{
+                                                height: '30px', borderStyle: 'solid', borderWidth: '0.1rem', borderLeftWidth: '0rem',
+                                                borderBottomColor: '#b4b4b4',
+                                                borderTopStyle: (isFirstOfArea) && "solid",
+                                                borderTopWidth: (isFirstOfArea) && "0.15rem",
+                                                borderTopColor: (isFirstOfArea) ? "black" : '#696969',
+                                            }}>
+                                            <td></td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+
+                        {chartDataLoaded && (
+                            <Chart
+                                width={isMobile ? "70vw" : "40vw"}
+                                height={chartHeight}
+                                chartType="Gantt"
+                                loader={<div>Loading Chart</div>}
+                                data={chartData}
+                                options={{
+                                    gantt: {
+                                        backgroundColor: {
+                                            fill: "transparent"
+                                        },
+                                        trackHeight: 15,
+                                        sortTasks: false,
+                                        palette: paleta,
+                                        shadowEnabled: false,
+                                        criticalPathEnabled: false,
+                                        barHeight: 10,
+                                        arrow: {
+                                            angle: 30,
+                                            length: 4,
+                                            width: 1,
+                                            color: 'black',
+                                            radius: 10
+                                        },
+                                        labelStyle: {
+                                            fontSize: 0.01
+                                        },
+                                        innerGridTrack: {
+                                            fill: 'transparent'
+                                        },
+                                        innerGridDarkTrack: {
+                                            fill: 'white'
+                                        },
+                                        innerGridHorizLine: {
+                                            strokeWidth: '0',
+                                        }
                                     },
-                                    labelStyle: {
-                                        fontSize: 0.01
-                                    },
-                                    innerGridTrack: {
-                                        fill: '#FFFFFF'
-                                    },
-                                    innerGridDarkTrack: {
-                                        fill: '#FFFFFF'
-                                    },
-                                    innerGridHorizLine: {
-                                        strokeWidth: '0',
-                                    }
-                                },
-                            }}
-                            style={{
-                                position: 'relative',
-                                zIndex: 1,
-                            }}
-                        />
-                    )}
+                                }}
+                                style={{
+                                    position: 'relative',
+                                    zIndex: 1,
+                                }}
+                            />
+                        )}
+                    </div>
                 </div>
-            </div>
             ) : (
                 <div className={styles.quickUpdate} style={{ marginBottom: '1rem' }}>
                     <div>
-                        No data available for this graph. <br/>
-                        Please register all items in <Link href="pags/timeline/timeline_plan">Estimated Timeline</Link><br/>
+                        No data available for this graph. <br />
+                        Please register all items in <Link href="pags/timeline/timeline_plan">Estimated Timeline</Link><br />
                         and use <Link href="pags/timeline/monitoring">Timeline Monitoring</Link>.
                     </div>
                 </div>
             )}
-            
+
         </div>
     );
 };
