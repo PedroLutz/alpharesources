6e965af49efe2a34942d6b0da0222dc9db56a275
Pedro Lutz <pedrolutz@protonmail.com>
2026-05-01 14:01:20 -0300

    renamed UseInformacaoData to temp to allow for name change

--- components/features/comunicacao/informacao/data/InformacaoContext.js
+++ components/features/comunicacao/informacao/data/InformacaoContext.js
@@ -1,5 +1,5 @@
 import {createContext, useContext} from 'react';
-import { useInformacaoData } from './useInformacaoData';
+import { useInformacaoData } from './temp';
 
 const InformacaoContext = createContext(null);
 
